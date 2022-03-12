c94bd8a3ef6401bd6c8f9285a2b9845d032d4f17
jxh0531 <jarred.harkness@gmail.com>
2022-03-12 16:56:31 +0100

    create product card

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ export default class Header extends Component {
   render() {
       return (
         <header>
-            <Navbar bg="dark" variant='dark' expand="lg">
+            <Navbar bg="secondary" variant='dark' expand="lg">
                 <Container>
                     <Navbar.Brand href="/">My Shop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart">Cart</Nav.Link>
-                            <Nav.Link href="/login">Sign In</Nav.Link>
+                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                            <Nav.Link href="/login"><i class="fa-solid fa-user"></i> Sign In</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
